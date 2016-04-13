33b0bbe701fbd9a1421b8b4fc466fc745c5ecfd2
Oscar <github@oscarb.se>
2016-04-13 11:34:36 +0200

    Added layout

--- app/src/main/java/se/oscarb/klotterplanket/MainActivity.java
+++ app/src/main/java/se/oscarb/klotterplanket/MainActivity.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+        Hur använder man Firebase med en Android app?
+        1. Lägg till Firebase i build.gradle
+            compile 'com.firebase:firebase-client-android:2.5.2'
+}
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
